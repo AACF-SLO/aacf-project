fe2517725c35ae9bfb97f57d1d9016993d245df2
Andrew <lorandrewed@gmail.com>
2019-09-04 07:58:30 -0700

    added google fonts, and updated siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,9 @@ module.exports = {
   plugins: [
       'gatsby-plugin-sass',
       'gatsby-plugin-react-helmet',
-
+      'gatsby-transformer-sharp',
+      'gatsby-plugin-sharp',
+      
       {
         resolve: 'gatsby-source-filesystem', /* source content from our md files */
         options: {
@@ -19,7 +21,13 @@ module.exports = {
             path: `${__dirname}/src/`
         }
       },
-      'gatsby-plugin-sharp',
+      {
+        resolve: 'gatsby-source-filesystem', /* source content from our img files */
+        options: {
+            name: 'images',
+            path: `${__dirname}/src/images/`
+        }
+      },
       {
         resolve: 'gatsby-transformer-remark',
         options: {
@@ -34,11 +42,26 @@ module.exports = {
             }
           ]
         }
+      },
+      {
+        resolve: `gatsby-plugin-google-fonts`,
+        options: {
+          fonts: [
+            `limelight`,
+            `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+            `Montserrat`,
+            `Lato`,
+            `Staatliches`
+          ],
+          display: 'swap'
+        }
       }
   ],
   siteMetadata: {
-    title: 'Full-Stack Bootcamp',
-    author: 'Andrew Lor'
+    title: 'AACF SLO',
+    author: 'Andrew Lor',
+    footer1: 'Made with God`s ❤️ by AACF SLO',
+    footer2: 'Copyright © 2019, Asian American Christian Fellowship at California Polytechnic State University - San Luis Obispo'
     //social media, emails, etc.
   }
 
