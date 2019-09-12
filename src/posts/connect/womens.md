2c8289bc8442d157776b17adbb0fcfc5c5ea6a9b
Andrew <lorandrewed@gmail.com>
2019-09-11 19:19:02 -0700

    Finished skeleton for Connect pages

---
+++ src/posts/connect/womens.md
@@ -0,0 +1,8 @@
+---
+title: "Women's"
+category: "Connect"
+description: "Women’s Ministry aims to cultivate a space where sisters can seek encouragement, prayer, and accountability. Through discipleship, women’s times, and retreat we aspire to build Christ-centered relationships where sisters can grow and support one another. "
+location: "San Luis Obispo"
+date: "During the school year"
+gif: "../../images/events/bball.gif"
+---
---
+++ src/templates/connect.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+import { graphql } from 'gatsby'
+
+import Layout from '../components/layout'
+
+import Head from '../components/head'
+
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+
+import eventsStyles from './events.module.scss'
+
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark(fields: {slug: {eq: $slug } }) {
+            frontmatter {
+                category
+                description
+                location
+                date
+                title
+                gif {
+                    publicURL
+                }
+            }
+            html
+            fields {
+                slug
+            }
+        }
+    }
+`
+//frontmatter: {title: {eq: "BLOG"}},  filter in markdownRemark
+
+// template for each blog page, used in gatsby-node.js
+const Connect = (props) => {
+    return (
+        <Layout>
+            <Head title={props.data.markdownRemark.frontmatter.title}/>
+            <Container fluid className={["my-5"].join(' ')}>
+                <Row>
+                    <Col xs={12} md={6} className={["mb-5 text-right order-1 order-md-2"].join(' ')}>
+                        <Row className={["justify-content-center"].join(' ')}>
+                            <Col xs={12} md={11}>
+                                <h1 className={eventsStyles.title}>{props.data.markdownRemark.frontmatter.title}</h1>
+                                <h3>{props.data.markdownRemark.frontmatter.description}</h3>
+                                <h2>When:</h2>
+                                <p>{props.data.markdownRemark.frontmatter.date}</p>
+                                <h2>Where:</h2>
+                                <p>{props.data.markdownRemark.frontmatter.location}</p>
+                                <a href="/">
+                                    <button className="" type="button">
+                                        SIGN UP
+                                    </button>
+                                </a>
+                            </Col>
+                        </Row>
+                        
+                        {/* <Button variant="primary" size="lg" > 
+                            SIGN UP
+                        </Button> */}
+                        {/* block, disabled*/}
+                    </Col>
+                    <Col xs={12} md={6} className={["order-2 order-md-1"].join(' ')}>
+                        <img
+                            src={props.data.markdownRemark.frontmatter.gif.publicURL}
+                            alt="thumbnail"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+            {/* <img
+            src={props.data.markdownRemark.frontmatter.gif.publicURL}
+            alt="thumbnail"
+            /> */}
+            {/* <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div> */}
+        </Layout>
+    )
+}
+
+export default Connect
