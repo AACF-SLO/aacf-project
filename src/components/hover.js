66181bb7d3bbd6201bbedf1dc62a77a75e6b7f91
Juyeong <jylee9817@gmail.com>
2019-09-11 18:32:01 -0700

    turning hover.scss into module

--- src/components/hover.js
+++ src/components/hover.js
@@ -9,52 +9,52 @@ import Image from "react-bootstrap/Image"
 import logo from "../images/AACF_LOGO_FINAL.png"
 import Container from "react-bootstrap/Container"
 
-import './hover.scss'
+import styles from './hover.module.scss'
 
 const Hover = () => {
     // TODO use react-router-bootstrap for effecient routing
     return (
-        <div className = "body">
-            <div className="container">
-                <div className = "card">
-                    <div className = "face face1">
-                        <div className = "content">
+        <div className = {styles.body}>
+            <div className ={styles.container}>
+                <div className = {styles.card}>
+                    <div className = {[styles.face, styles.face1].join('')}>
+                        <div className = {styles.content}>
                             <img src = "design.png"></img>
                             <h3>Event 1</h3>
                         </div>
                     </div>
-                    <div className = "face face2">
-                        <div className = "content">
+                    <div className = {[styles.face, styles.face2].join("")}>
+                        <div className = {styles.content}>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                             Ratione hic nihil illo optio expedita earum</p>
                             <a href = "#">More Details</a>
                         </div>
                     </div>
                 </div>
-                <div className = "card">
-                    <div className = "face face1">
-                        <div className = "content">
+                <div className = {styles.card}>
+                    <div className = {[styles.face, styles.face1].join('')}>
+                        <div className = {styles.content}>
                             <img src = "design.png"></img>
                             <h3>Event 2</h3>
                         </div>
                     </div>
-                    <div className = "face face2">
-                        <div className = "content">
+                    <div className = {[styles.face, styles.face2].join('')}>
+                        <div className = {styles.content}>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                             Ratione hic nihil illo optio expedita earum</p>
                             <a href = "#">More Details</a>
                         </div>
                     </div>
                 </div>
-                <div className = "card">
-                    <div className = "face face1">
-                        <div className = "content">
+                <div className = {styles.card}>
+                    <div className = {[styles.face, styles.face1].join('')}>
+                        <div className = {styles.content}>
                             <img src = "design.png"></img>
                             <h3>Event 3</h3>
                         </div>
                     </div>
-                    <div className = "face face2">
-                        <div className = "content">
+                    <div className = {[styles.face, styles.face2].join('')}>
+                        <div className = {styles.content}>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                             Ratione hic nihil illo optio expedita earum</p>
                             <a href = "#">More Details</a>
