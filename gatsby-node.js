b516231388f99150cd1cdf9169e29cc05db11a30
Andrew <lorandrewed@gmail.com>
2019-09-11 19:16:38 -0700

    Created Event pages using template

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,6 @@ module.exports.onCreateNode = ({node, actions}) => {
         slug = path.basename(node.fileAbsolutePath, '.md')
 
         // console.log('@@@@@@@@@',slug)
-
         // created a new field in node to hold file slug
         // Adding our slug value to the slug key value pair inside our .md nodes
         createNodeField({
@@ -27,6 +26,9 @@ module.exports.createPages = async ({graphql, actions}) => {
     const blogTemplate = path.resolve('./src/templates/blog.js')//curent location to destination
     // const aboutTemplate = path.resolve('./src/templates/about.js') not gonna be making multiple /about/... pages
     // will use for events/connect pages
+    const eventsTemplate = path.resolve('./src/templates/events.js')
+
+    const connectTemplate = path.resolve('./src/templates/connect.js')
 
     const res = await graphql(`
         query {
@@ -35,6 +37,9 @@ module.exports.createPages = async ({graphql, actions}) => {
                     node {
                         fields {
                             slug
+                        }
+                        frontmatter {
+                            category
                         }
                     }
                 }
@@ -46,14 +51,46 @@ module.exports.createPages = async ({graphql, actions}) => {
     // will use a conditional here to path to each subpage:
     // res.data.allMarkdownRemark.edges.node.frontmatter.category .... path: `/events/... with different template 
     res.data.allMarkdownRemark.edges.forEach((edge) => {
-        createPage({
-            component: blogTemplate,
-            path: `/blog/${edge.node.fields.slug}`,
-            context: {
-                slug: edge.node.fields.slug
-            }
-        })
+        // console.log(edge.node)
+        if (edge.node.frontmatter.category === 'Events') {
+            // console.log(edge.frontmatter.category)
+            createPage({
+                component: eventsTemplate,
+                path: `/${edge.node.fields.slug}`,
+                context: {
+                    slug: edge.node.fields.slug
+                }
+            })
+        }
+        else if (edge.node.frontmatter.category === 'BLOG') {
+            createPage({
+                component: blogTemplate,
+                path: `/blog/${edge.node.fields.slug}`,
+                context: {
+                    slug: edge.node.fields.slug
+                }
+            })
+        }
+        else if (edge.node.frontmatter.category === 'Connect') {
+            createPage({
+                component: connectTemplate,
+                path: `/${edge.node.fields.slug}`,
+                context: {
+                    slug: edge.node.fields.slug
+                }
+            })
+        }
     })
+
+    // res.data.allMarkdownRemark.edges.forEach((edge) => {
+    //     createPage({
+    //         component: blogTemplate,
+    //         path: `/blog/${edge.node.fields.slug}`,
+    //         context: {
+    //             slug: edge.node.fields.slug
+    //         }
+    //     })
+    // })
 
     // 2. get markdown data
     // 3. create new pages
