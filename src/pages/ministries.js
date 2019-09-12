dfa3cadd9febdc4028f74c45b999e6abc6187229
Andrew <lorandrewed@gmail.com>
2019-09-11 19:19:31 -0700

    finished skeleton for ministries page

---
+++ src/pages/ministries.js
@@ -0,0 +1,137 @@
+// TODO: this page is better as a template since there will be variable leaders
+
+
+import React from "react"
+
+import Layout from '../components/layout'
+
+import { Link, graphql, useStaticQuery } from "gatsby"
+
+import ministryStyles from './ministries.module.scss'
+
+import Head from "../components/head"
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+import cross from '../images/cross-about.jpg'
+
+const Ministries = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark(filter: {frontmatter: {category: {eq: "Ministries"}}}) {
+              edges {
+                node {
+                  frontmatter {
+                    title
+                    ministries {
+                      ministry {
+                        people {
+                          person {
+                            detail
+                            name
+                            photo {
+                                publicURL
+                            }
+                          }
+                        }
+                        responsibility
+                        type
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+    `)
+
+    // console.log(data)
+    // this is the blog page that holds blog list
+    return (
+        <Layout>
+            <Head title="Ministries"/>
+            <div className="my-5">
+                <Jumbotron fluid style={{backgroundImage: `url(${cross})`}} className={[ministryStyles.heading, "text-center"].join(' ')}>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h1>AACF SLO MINISTRIES</h1>
+                                <p>Find who's serving AACF SLO below!</p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Jumbotron>
+                {data.allMarkdownRemark.edges[0].node.frontmatter.ministries.map((ministry) => {
+                    return (
+                        <div>
+                            <Container fluid className="mb-5 text-center">
+                                <Row className="">
+                                    <Col>
+                                        <h2>{ministry.ministry.type}</h2>
+                                    </Col>
+                                </Row>
+                                <Row className="justify-content-center">
+                                    <Col xs={12} md={8}>
+                                        <p>{ministry.ministry.responsibility}</p>
+                                        <hr/>
+                                    </Col>
+                                </Row>
+                                <Row className="d-flex justify-content-start">
+                                {ministry.ministry.people.map((childOfGod) => {
+                                    return (
+                                        <React.Fragment key={childOfGod.uid}>
+                                            <Col md={4} className="px-5 py-2 d-flex justify-content-center">
+                                                <Card>
+                                                    <Card.Img variant="top" src={childOfGod.person.photo.publicURL} />
+                                                    <Card.Body>
+                                                        <Card.Title>{childOfGod.person.name}</Card.Title>
+                                                        <Card.Text>{childOfGod.person.detail}</Card.Text>
+                                                    </Card.Body>
+                                                </Card>
+                                            </Col>
+                                        </React.Fragment>
+                                    )
+                                })} 
+                                    
+                                </Row>
+                            </Container>
+                        </div>
+                    )
+                })}
+            </div>
+            {/* <h1>Ministries page</h1>
+            <ol className={blogStyles.posts}>
+                {data.allMarkdownRemark.edges[0].node.frontmatter.ministries.map((m) => {
+                    return (
+                        <li key={m.uid}>
+                            <p>{m.uid}hello</p>
+                            
+                            
+                        </li>
+                    )
+                })}
+            </ol> */}
+        </Layout>
+    )
+}
+// LOOK AT REACT-KEY-INDEX LIB TO FIX KEY ERRORS
+export default Ministries
+
+/* <Link to={`/blog/${edge.node.fields.slug}`}>
+                                <h2>{edge.node.frontmatter.title}</h2>
+                                hi
+                                <p>{edge.node.frontmatter.date}</p>
+                            </Link> */
+/* <h1>{m.ministry.type}</h1>
+{m.ministry.people.map((p) => {
+    return (
+        <div key={p.id}>
+            <p>{p.person.name}</p>
+            <img src={p.person.photo.childImageSharp.fluid.base64}/>
+        </div>
+    )
+})} */
---
+++ src/posts/ministries.md
@@ -0,0 +1,104 @@
+---
+category: "Ministries"
+title: "Ministries"
+ministries: 
+    - ministry: 
+        type: "large group"
+        responsibility: "responsibilities here"
+        people: 
+            - person:
+                name: "Martin Jiang"
+                detail: "3rd year, computer engineering"
+                photo: "../images/ministries/adam-m-1.jpg"
+            - person:
+                name: "Calvin Wang"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "4rd year, civil engineering"
+            - person: 
+                name: "Lawrence Lai"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "2nd year, electrical engineering"
+            - person: 
+                name: "Naryung Kim"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "4th year, child development"
+            - person: 
+                name: "Peter Mar"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "3rd year, food science"
+    - ministry:
+        type: "outreach"
+        responsibility: "responsibilities here"
+        people: 
+            - person:
+                name: "Jonathan Date-Chong"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "4th year, computer science"
+            - person: 
+                name: "Andre Tran"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "4th year, business"
+            - person: 
+                name: "Connor Lee"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "2nd year, mechanical engineering"
+            - person: 
+                name: "Kevin Yu"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "4th year, computer engineering"
+            - person: 
+                name: "Rachel Wang"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "4th year, business"
+    - ministry:
+        type: "small groups"
+        responsibility: "responsibilities here"
+        people:
+            - person: 
+                name: "Jaclyn Shimada"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "3rd year, electrical engineering"
+            - person: 
+                name: "Charley Zhang"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "3rd year, experience industry management"
+            - person: 
+                name: "Stanley Suh"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "3rd year, business"
+    - ministry: 
+        type: "women's"
+        responsibility: "responsibilities here"
+        people:
+            - person: 
+                name: "Michelle Kang"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "3rd year, graphic communications"
+            - person: 
+                name: "Courtney Koyama"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "4th year, business"
+            - person: 
+                name: "Monica Andres"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "3rd year, computer science"
+    - ministry:
+        type: "men's"
+        responsibility: "responsibilities here"
+        people:
+            - person: 
+                name: "Isiah Kim"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "3rd year, english"
+            - person: 
+                name: "Justin Wang"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "4th year, business"
+            - person: 
+                name: "Nick Desimone"
+                photo: "../images/ministries/adam-m-1.jpg"
+                detail: "3rd year, mechanical engineering"
+advisor: "David Janzen"
+---
+can make each ministry a key/value pair or a list with '-'
+will be able to add image inside each person
