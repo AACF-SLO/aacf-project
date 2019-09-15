e7a59435740201d5d632864d0262a058779ba154
Andrew <lorandrewed@gmail.com>
2019-09-14 22:34:55 -0700

    fixed .map() Key error with index as key

--- src/pages/churches.js
+++ src/pages/churches.js
@@ -74,9 +74,11 @@ const Churches = () => {
             </Jumbotron>
             <Container className="mb-5 text-center"> {/* Can be fluid*/}
                 <Row className="d-flex justify-content-center">
-                    {data.allMarkdownRemark.edges[0].node.frontmatter.churches.map((church) => {
+                    {data.allMarkdownRemark.edges[0].node.frontmatter.churches.map((church, index) => {
                         return (
-                            <React.Fragment>
+                            <React.Fragment key={index}>
+                                {/* {console.log(index)} */}
+                                {/* TODO: Should use react-key-index instead */}
                                 <Col md={4} lg={3} className="px-2 py-2 d-flex justify-content-center">
                                     <a href={church.church.link} target="_blank" rel="noopener noreferrer">
                                         <Card style={{ width: '14rem' }} >
