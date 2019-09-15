891ae5ae2fe1497ed2f672beb22fdceb69c8a7eb
Andrew <lorandrewed@gmail.com>
2019-09-14 20:53:55 -0700

    404 page updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,14 +3,25 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import Head from '../components/head'
 
 const NotFound = () => {
     return (
         <Layout>
             <Head title="404"/>
-            <h1>Page Not Found</h1>
-            <p><Link to="/">Head home</Link></p>
+            <Container >
+                <Row>
+                    <Col className="text-center">
+                        <h1>Page Not Found</h1>
+                        <p><Link to="/">Head home</Link></p>
+                    </Col>
+                </Row>
+            </Container>
+            
         </Layout>
     )
 }
