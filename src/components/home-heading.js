e079e01d8597d48d918345fca2e17d91a0d94f42
Andrew <lorandrewed@gmail.com>
2019-09-16 12:26:32 -0700

    updated Home page from docs

--- src/components/home-heading.js
+++ src/components/home-heading.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { LinkContainer } from 'react-router-bootstrap'
 import { Link } from 'gatsby'
 
 
@@ -10,32 +9,32 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 
 import homeHeading from './home-heading.module.scss'
-import cross from '../images/cross.jpg'
+import calpoly from '../images/cpstock.jpg'
 
 
 
 const Heading = () => {
     return  (
-        <Jumbotron fluid style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${cross})`}} className={[homeHeading.section, "text-center"].join(' ')}>
+        <Jumbotron fluid style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${calpoly})`}} className={[homeHeading.section, "text-center"].join(' ')}>
                 <Container>
                     <Row>
                         <Col>
-                            <h1>Stay Tuned for Upcoming Events!</h1>
+                            <h1>Upcoming Events!</h1>
                         </Col>
                     </Row>
                     <Row className={["justify-content-around d-flex mt-4"].join(' ')} >  {/* className={["justify-content-around d-flex"].join(' ')} */}
                         <Col xs={12} md={6}>
-                            <Link to="/outreach"><Button size="lg" block variant="primary">Freshmen Outreach</Button></Link>
+                            <Link to="/outreach"><Button className={homeHeading.button} size="lg" block variant="primary">FRESHMEN OUTREACH</Button></Link>
                         </Col>
                     </Row>
                     <Row className={["justify-content-around d-flex mt-4"].join(' ')} >  {/* className={["justify-content-around d-flex"].join(' ')} */}
                         <Col xs={12} md={6}>
-                            <Link to="/large-group"><Button size="lg" block variant="primary">Large Group</Button></Link>
+                            <Link to="/large-group"><Button className={homeHeading.button} size="lg" block variant="primary">LARGE GROUP</Button></Link>
                         </Col>
                     </Row>
                     <Row className={["justify-content-around d-flex mt-4"].join(' ')} >  {/* className={["justify-content-around d-flex"].join(' ')} */}
                         <Col xs={12} md={6}>
-                            <Link to="/small-group"><Button size="lg" block  variant="primary">Small Group</Button></Link>
+                            <Link to="/small-group"><Button className={homeHeading.button} size="lg" block  variant="primary">SMALL GROUP</Button></Link>
                         </Col>
                     </Row>
                    
