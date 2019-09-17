2f9e93122f492f6fea2d1decc70ab8a8bdc477ef
Andrew <lorandrewed@gmail.com>
2019-09-16 21:13:48 -0700

    updated Upcoming Events in Home

--- src/components/home-heading.js
+++ src/components/home-heading.js
@@ -10,12 +10,13 @@ import Button from 'react-bootstrap/Button'
 
 import homeHeading from './home-heading.module.scss'
 import calpoly from '../images/cpstock.jpg'
+import upcoming from '../images/UpcomingEvents.jpg'
 
 
 
 const Heading = () => {
     return  (
-        <Jumbotron fluid style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${calpoly})`}} className={[homeHeading.section, "text-center"].join(' ')}>
+        <Jumbotron fluid style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${upcoming})`}} className={[homeHeading.section, "text-center"].join(' ')}>
                 <Container>
                     <Row>
                         <Col>
