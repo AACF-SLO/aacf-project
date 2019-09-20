8b9b7bb0e7fa5c6b1d45c047bec82ebc2cf5eb63
Andrew <lorandrewed@gmail.com>
2019-09-19 23:44:00 -0700

    smaller IG logo and aacf logo.

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+
 // import { Link, graphql, useStaticQuery } from "gatsby"
 // graphql and useStaticQuery needed to use graphql
 import { Link } from "gatsby"
@@ -8,7 +9,7 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import NavDropdown from "react-bootstrap/NavDropdown"
 import Image from "react-bootstrap/Image"
-import logo from "../images/AACF_LOGO_FINAL.png"
+import logo from "../images/AACF_LOGO_FINAL_copy.png"
 // import Container from "react-bootstrap/Container"
 // import Button from "react-bootstrap/Button"
 
@@ -23,42 +24,10 @@ const Header = () => {
     //querying to get title below
     // const data = useStaticQuery(graphql`
     //     query {
-    //         site {
-    //             siteMetadata {
-    //                 title
-    //             }
-    //         }
+            
     //     }
     // `)
 
-    // return (
-    //     <header className={headerStyles.header}>
-    //         <h1>
-    //             <Link className={headerStyles.title} to="/">
-    //                 {data.site.siteMetadata.title}
-    //             </Link>
-    //         </h1>
-    //         <nav>
-    //             <ul className={headerStyles.navList}>
-    //                 <li>
-    //                     <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
-    //                 </li>
-    //                 <li>
-    //                     <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
-    //                 </li>
-    //                 <li>
-    //                     <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
-    //                 </li>
-    //                 <li>
-    //                     <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
-    //                 </li>
-    //             </ul>
-    //         </nav>
-    //     </header>
-    // )
-
-
-    // TODO use react-router-bootstrap for effecient routing
     return (
         <Navbar sticky="top" className={headerStyles.navbarExpandLg} collapseOnSelect expand="lg" bg="light" variant="light">
             <Navbar.Brand className={headerStyles.navbarBrand} >
