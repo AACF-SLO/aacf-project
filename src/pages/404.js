21e148ec527ae8fab8497719e4cd9e5ccc804008
Monica <monicasjunior@gmail.com>
2020-09-07 02:00:50 -0700

    updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,19 +9,27 @@ import Col from 'react-bootstrap/Col'
 
 import Head from '../components/head'
 
+import { useStaticQuery, graphql } from 'gatsby'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Img from 'gatsby-image'
+import sleep from '../images/sleep.png'
+import {Image} from 'react-bootstrap';
+
+import errorStyles from './404.module.scss'
+
+
 const NotFound = () => {
+
     return (
         <Layout>
             <Head title="404"/>
-            <Container >
-                <Row>
-                    <Col className="text-center">
-                        <h1>Page Not Found</h1>
-                        <p><Link to="/">Head home</Link></p>
-                    </Col>
-                </Row>
-            </Container>
-            
+            <div className={[errorStyles.heading," image"].join(' ')}>
+
+               <h2>404 Error. Page not found.<br />ZZZZZZZZ</h2>
+
+                  <img src={sleep} alt="" />
+
+            </div>
         </Layout>
     )
 }
